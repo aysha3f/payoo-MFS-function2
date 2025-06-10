0c00b52010e81fa83c1f193fc28b4e46164d09c0
Recap basic structure and adding event handler <tahiaysha220@gmail.com>
2025-06-11 00:26:52 +0600

    Create Reusable function to get input field value

--- js/addMoney.js
+++ js/addMoney.js
@@ -9,10 +9,21 @@
 
 
 
-document.getElementById('btn-add-money').addEventListener(click,function(event){
+document.getElementById('btn-add-money').addEventListener('click',function(event){
 event.preventDefault();
 
+
+console.log('add money button clicked')
+
+// getInputFieldValueId();
+// const addMoney = getInputFieldValueId();
+// console.log('add money value',addMoney);
 // const addMoney = document.getElementById('input-add-money').value;
 // const addMoneyNumber = parseFloat(addMoney);
+
+
+const addMoney = getInputFieldValueId('input-add-money')
+const pinNumber = getInputFieldValueId('input-pin-number')
+console.log('add money with parameter',addMoney, pinNumber)
 
 })
--- js/utilities.js
+++ js/utilities.js
@@ -2,6 +2,13 @@
  * common shared functions here
  */
 
-function getInputFieldValueId(){
-    console.log('will get input field value id')
+// function getInputFieldValueId(){
+//     console.log('will get input field value id')
+//     const addMoney = document.getElementById('input-add-money').value;
+// return addMoney;
+// }
+
+function getInputFieldValueId(id){
+const inputValue = document.getElementById(id).value;
+return inputValue;
 }
